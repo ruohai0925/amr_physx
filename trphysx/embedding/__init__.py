9dd43eced84e49fb399f1bb435a2f7ee7039db0a
yzeng <yzeng@altair.com>
2023-04-10 15:59:34 -0400

    add the pyreverse generated png files

--- trphysx/embedding/__init__.py
+++ trphysx/embedding/__init__.py
@@ -2,6 +2,4 @@ __all__ = ['AutoEmbeddingModel', 'CylinderEmbedding', 'GrayScottEmbedding', 'Lor
 
 from .embedding_auto import AutoEmbeddingModel
 from .embedding_cylinder import CylinderEmbedding
-from .embedding_grayscott import GrayScottEmbedding
-from .embedding_lorenz import LorenzEmbedding
 from .embedding_model import EmbeddingModel, EmbeddingTrainingHead
--- trphysx/viz/__init__.py
+++ trphysx/viz/__init__.py
@@ -1,5 +1,3 @@
 from .viz_auto import AutoViz
 from .viz_cylinder import CylinderViz
-from .viz_grayscott import GrayScottViz
-from .viz_lorenz import LorenzViz
 from .viz_model import Viz
